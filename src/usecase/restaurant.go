f589289995f44102d4fe77e725ab34eaaff5b160
hisamitsu <ryohei.hisamitsu@ncdc.co.jp>
2021-08-10 00:45:03 +0900

    検索API一旦完成

--- src/usecase/restaurant.go
+++ src/usecase/restaurant.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"os"
 	"restaurant-record/domain/model"
+	"strconv"
+	"strings"
 
 	"googlemaps.github.io/maps"
 )
@@ -22,12 +24,17 @@ func NewRestaurantUsecase() RestaurantUsecase {
 
 // 近隣の飲食店を返却
 func (ru *restaurantUsecase) FindNear(location string) (*[]model.Restaurant, error) {
-	mapLocation := maps.LatLng{Lat: 34.39339788681548, Lng: 132.42305207662446}
-
+	// クライアントの生成
 	c, err := maps.NewClient(maps.WithAPIKey(os.Getenv("API_KEY")))
 	if err != nil {
 		log.Fatalf("fatal error: %s", err)
 	}
+
+	// クエリの生成
+	splittedLocation := strings.Split(location, ",")
+	lat, _ := strconv.ParseFloat(splittedLocation[0], 64)
+	lng, _ := strconv.ParseFloat(splittedLocation[1], 64)
+	mapLocation := maps.LatLng{Lat: lat, Lng: lng}
 	r := &maps.NearbySearchRequest{
 		Location: &mapLocation,
 		Radius:   200,
@@ -35,14 +42,14 @@ func (ru *restaurantUsecase) FindNear(location string) (*[]model.Restaurant, err
 		Language: "ja",
 	}
 
+	// リクエスト送信
 	res, err := c.NearbySearch(context.Background(), r)
 	if err != nil {
 		log.Fatalf("fatal error: %s", err)
 	}
 
-	// ドメインオブジェクトの配列に詰め替え
+	// 結果をドメインオブジェクトの配列に詰め替え
 	var restaurants []model.Restaurant
-
 	for _, v := range res.Results {
 		restaurants = append(restaurants, model.Restaurant{
 			Name: v.Name,
